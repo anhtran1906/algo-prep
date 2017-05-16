8fe25d3eebd7644c67946c2032b342c111d79640
Anh Tran <trana1906@gmail.com>
2017-05-16 20:48:55 +0700

    added 2 more solutions for rotateArrq

--- src/Solution.java
+++ src/Solution.java
@@ -18,4 +18,51 @@ public class Solution {
         }
         System.arraycopy(newArr,0,nums,0,nums.length);
     }
+    /*
+        This answer gives O(n*k) solution. Takes the idea of bubble sort
+     */
+    public static void rotate2(int[] nums, int k) {
+        if(nums == null || k < 0){
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        if(k > nums.length){
+            k = k - nums.length;
+        }
+        for(int i = 0; i < k; i++){
+            for(int j = nums.length-1; j > 0; j--){
+                int temp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = temp;
+            }
+        }
+    }
+
+    public static void rotate3(int[] nums, int k){
+        if(nums == null || k < 0){
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        if(k > nums.length){
+            k = k - nums.length;
+        }
+
+        //length of the first part
+        int arrLength = nums.length - k;
+        reverse(nums,0,arrLength-1);
+        reverse(nums, arrLength,nums.length-1);
+        reverse(nums,0,nums.length-1);
+    }
+
+    private static void reverse(int[] nums, int start, int end){
+        if(nums == null || nums.length == 1){
+            return;
+        }
+        while(start < end){
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@
 public class Test {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7};
-        Solution.rotate(arr,3);
+        Solution.rotate3(arr,3);
         //sout-
         for(int i: arr){
             System.out.println(i);
